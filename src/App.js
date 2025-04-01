f97b6a35bb345b539342cb599c83c52b9eebd267
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2025-04-01 15:23:25 +0000

    Add routing and react-icons support to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import MiniDataProjects from './components/miniDataProjects';
+import Contacts from './components/Contacts';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/miniDataProjects" element={<MiniDataProjects />} />
+        <Route path="/contact" element={<Contacts />} />
+      </Routes>
     </div>
   );
 }
